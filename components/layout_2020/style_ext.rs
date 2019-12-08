8667cf9d6037acb11301f387ea3a86a44f4b41f9
Simon Sapin <simon.sapin@exyr.org>
2019-12-08 11:41:29 +0100

    Fix a “Accessing content size that was not requested” panic
    Percentage `width` are treated as `auto` for the purpose of
    min/max-content computation, so they also need to be considered
    when testing “wether width is auto”

--- components/layout_2020/style_ext.rs
+++ components/layout_2020/style_ext.rs
@@ -46,7 +46,7 @@ pub(crate) enum DisplayInside {
 pub(crate) trait ComputedValuesExt {
     fn writing_mode(&self) -> (WritingMode, Direction);
     fn writing_mode_is_horizontal(&self) -> bool;
-    fn inline_size_is_auto(&self) -> bool;
+    fn inline_size_is_length(&self) -> bool;
     fn inline_box_offsets_are_both_non_auto(&self) -> bool;
     fn box_offsets(&self) -> flow_relative::Sides<LengthPercentageOrAuto>;
     fn box_size(&self) -> flow_relative::Vec2<LengthPercentageOrAuto>;
@@ -72,14 +72,14 @@ impl ComputedValuesExt for ComputedValues {
         }
     }
 
-    fn inline_size_is_auto(&self) -> bool {
+    fn inline_size_is_length(&self) -> bool {
         let position = self.get_position();
         let size = if self.writing_mode_is_horizontal() {
             position.width
         } else {
             position.height
         };
-        matches!(size, Size::Auto)
+        matches!(size, Size::LengthPercentage(lp) if lp.0.as_length().is_some())
     }
 
     fn inline_box_offsets_are_both_non_auto(&self) -> bool {
